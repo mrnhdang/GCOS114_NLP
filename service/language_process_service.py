60b84ea4434afd4004ff14030f76418a643bd6cc
O2F-IT\huudang.nguyen_amari <dang.nguyen.con@beintl.com>
2024-11-01 15:57:19 +0700

    1/11/2024: Clean and tokenize data from file

--- service/language_process_service.py
+++ service/language_process_service.py
@@ -1,6 +1,12 @@
 import os
+import re
+import nltk
+# nltk.download('punkt_tab')
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
 
-stopwords_dir = './file/stopwords'
+stopwords_file = './file/stopwords/english_stopwords'
+
 
 class LanguageProcessService:
 
@@ -9,6 +15,7 @@ class LanguageProcessService:
         with open(file, 'r', encoding='utf8') as f:
             for line in f:
                 line = line.strip()
+                line = line.lower()
                 data.append(line)
         return data
 
@@ -23,21 +30,45 @@ class LanguageProcessService:
         return list_of_file
 
     def extract_stopwords(self):
-        data = []
-        dir1 = self.get_directory_file(stopwords_dir)
-        for file in dir1:
-            stopword = self.extract_from_file(file)
-            print(f'stopword: {stopword}')
-
-            data.append(stopword)
-        return data
+        stopword = self.extract_from_file(stopwords_file)
+        return stopword
 
     def clean_data(self, data):
-        stopword = self.extract_stopwords()
+        clean_data = []
+        stopwords = self.extract_stopwords()
+        print(f'stopwords: {stopwords}')
 
+        # remove XML and HTML tag from text
+        for text in data:
+            soup = BeautifulSoup(text, "html.parser")
+            clean_text = soup.get_text()
+
+            # check empty string
+            if clean_text.strip():
+                # remove Punctuation
+                clean_text = re.sub('\W+', ' ', clean_text)
+
+                tokens = word_tokenize(clean_text)
+
+                # remove stopwords
+                filtered_tokens = [token for token in tokens if token not in stopwords]
+                print(f'clean_text: {filtered_tokens}')
+                clean_data.append(filtered_tokens)
+
+        return clean_data
 
     def process_data(self, directory):
-        dir3 = self.get_directory_file(directory)
-        for file in dir3:
+        clean_data_arr = []
+
+        # extract data from file
+        for file in self.get_directory_file(directory):
+            # extract_from_file
             data = self.extract_from_file(file)
-            print(f'data: {data}')
+
+            # clean_data
+            clean_data = self.clean_data(data)
+
+            print(f'data: {clean_data}')
+            clean_data_arr.append(clean_data)
+
+        print(f'clean_data_arr: {clean_data_arr}')
